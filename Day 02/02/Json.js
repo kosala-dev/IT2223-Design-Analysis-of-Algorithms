d80078c74496819fbfa34dc8ab45860f54896ade
Kosala Madhushan <69857444+kosala-dev@users.noreply.github.com>
2025-03-18 15:33:16 +0530

    Add files via upload

---
+++ Day 02/02/Json.js
@@ -0,0 +1,21 @@
+//JSON
+let student = {regno: '2021/ICT/36', name: "Kosala Madushan", age: 22, Course: 'Bsc.IT', Skills: ['Java', 'C++', 'JS']}
+console.log(student)
+console.log(student.name)
+console.log()
+
+let students = [
+    {regno: '2021/ICT/36', name: "Kosala Madushan", age: 23, Course: 'Bsc.IT', Skills: ['Java', 'C++', 'JS']},
+    {regno: '2021/ICT/46', name: "Malidu Sasanka", age: 24, Course: 'Bsc.IT', Skills: ['Java', 'C++', 'JS']},
+    {regno: '2021/ICT/34', name: "Chamara weerasinghe", age: 24, Course: 'Bsc.IT', Skills: ['Java', 'C++', 'JS']}
+]
+console.log(students)
+console.log()
+
+//Exercise 01
+//Define 10 students JSON
+//store it in an array
+//find the female students
+//find the students who are following IT course
+//find the max and average GPA among the students
+
